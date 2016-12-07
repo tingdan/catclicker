1dba6d1588665dffd74430766dd4deb4edcea29c
tingdan <ting.danni@gmail.com>
2016-12-07 20:17:37 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var cats = [{name:'Andy',picLocate:'bigcat.jpg',clicked:0 },
             {name:'Lizzy',picLocate:'cat3.jpg',clicked:0},
             {name:'Benny',picLocate:'cat4.jpg',clicked:0},
             {name:'Jorge',picLocate:'cat5.jpg',clicked:0}];
+var index;
 var catHtml = '';
 var catListHtml = '<h3>Choose your cat</h3><div id="catList"><ul>';
+
 //show cat pics in div
 for(var i=0;i<cats.length;i++){
           catListHtml += '<li>'+cats[i].name+'</li>'
@@ -16,12 +18,12 @@ for(var i=0;i<cats.length;i++){
 
 
 
-$('li').one('click',function(){
-    var index = $('li').index(this);
+$('li').on('click',function(){
+    index = $('li').index(this);
     var catname = $(this).text();
-    var showHtml = catname+'<img class ='+catname+ ' src="'+cats[index].picLocate+'">';
-    $('#catpic').append(showHtml);
-    $('#output').append(catname +'  clicked: <span class='+catname+'>0</span><br>');
+    var showHtml = '<img class ='+catname+ ' src="'+cats[index].picLocate+'">';
+    $('#catpic').html(showHtml);
+    $('#output').html(catname +'  clicked: <span>'+cats[index].clicked+'</span><br>');
 });
 
 
@@ -32,11 +34,13 @@ $('#catpic').on('click','img',function(){
 
   //  console.log(c);
   var getname =  $(this).attr('class');
-  var select = 'span.'+getname;
-  var parseClick = parseInt($(select).text());
-  parseClick++;
-  console.log(select);
-  $(select).html(parseClick);
+//  var select = 'span.'+getname;
+//  var parseClick = parseInt($(select).text());
+  var getNumClick = cats[index].clicked;
+  getNumClick = getNumClick+1;
+   cats[index].clicked = getNumClick;
+  //console.log(select);
+  $('span').html(getNumClick);
   //$('span .'+getname).html('bozo');
   //  $(this).next().html('Clicked : '+ click);
 
